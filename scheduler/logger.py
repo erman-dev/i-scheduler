c16746c2ba7369b59149bc686a0141d9199aecd2
Roman Krček <roman.krcek@tietoevry.com>
2025-08-22 11:25:51 +0200

    fix logger handlers

--- scheduler/logger.py
+++ scheduler/logger.py
@@ -8,11 +8,12 @@ def get_logger(name: str):
     """
     logger = logging.getLogger(name)
     logger.setLevel(logging.INFO)
-    handler = logging.StreamHandler(sys.stdout)
-    handler.setLevel(logging.INFO)
-    formatter = logging.Formatter(
-        '%(asctime)s | %(name)25s | %(levelname)6s | %(message)s'
-    )
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
+    if not logger.handlers:
+        handler = logging.StreamHandler(sys.stdout)
+        handler.setLevel(logging.INFO)
+        formatter = logging.Formatter(
+            '%(asctime)s | %(name)25s | %(levelname)6s | %(message)s'
+        )
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
     return logger
